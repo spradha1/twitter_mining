3a5b0c617428f2544520b6b1c8f38bb458e50a90
spradha1 <pradhanang.sanjiv@gmail.com>
2017-03-02 13:05:42 -0600

    streaming tweets by text filter

---
+++ twitter_streaming.py
@@ -0,0 +1,35 @@
+#Retrieves data of tweets in json filtering by words chosen
+
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+#Variables that contains the user credentials to access Twitter API 
+access_token = "3007046359-FqGStlTSiRB2une5rHdzYzoblEnjvkdCFVtbzht"
+access_token_secret = "81zqPgjR6BUIkAyjnzTtZvabKcafrTvV6gb5v9sFJci4q"
+consumer_key = "M7N7E0Wp0Mo57bYeDU5h8Zlcs"
+consumer_secret = "htS82aShch7D0eCUgDMeBQurkxH1oZGjnwD1DUUDipH11TYV6T"
+
+
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+    stream.filter(track=['python', 'javascript', 'ruby'])
